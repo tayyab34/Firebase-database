c52f4fb2adab6d2eddcd4242b1f78ce97703b91b
tayyab34 <ahmedtayyab034@gmail.com>
2022-05-26 10:25:13 +0500

    Firebase Database

---
+++ Firebase Database/Assets/User.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User 
+{
+    public string name;
+    public int gold;
+
+    public User(string Name, int Gold)
+    {
+        this.name = Name;
+        this.gold = Gold;
+    }
+}
